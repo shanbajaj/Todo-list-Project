a2f5ed0fbed0ea877b947a5150b5ac55943f4312
shanbajaj <shantanubajaj22@gmail.com>
2020-08-15 18:29:39 +0530

    initial commit

---
+++ myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Task(models.Model):
+    def __str__(self):
+        return self.name
+    name=models.CharField(max_length=100)
+    priority=models.IntegerField()
+# Create your models here.
